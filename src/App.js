78eb820353fe7ddc8599b1ecf79b9bd319c50dc2
arnab072000 <arnab11kr@gmail.com>
2023-12-22 17:45:45 +0530

    "stopwatch commit"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { useEffect, React, useState } from "react";
+//import "./styles.css";
+
+export default function App() {
+  const [isActivated, setIsActivated] = useState(false);
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    let intervalID;
+
+    if (isActivated) {
+      intervalID = setInterval(() => {
+        setTimer((prevTime) => prevTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(intervalID);
+    }
+    return () => clearInterval(intervalID);
+  }, [isActivated]);
+
+  const startStop = () => {
+    setIsActivated((prevIsActivated) => !prevIsActivated);
+  };
+
+  const reset = () => {
+    setIsActivated(false);
+    setTimer(0);
+  };
+
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes} : ${
+      remainingSeconds < 10 ? "0" : ""
+    } ${remainingSeconds}`;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stopwatch</h1>
+      <h4> Time : {formatTime(timer)}</h4>
+      <div className='button'>
+       <button onClick={startStop}>{isActivated ? "Stop" : "Start"}</button>
+       <button onClick={reset}>Reset</button>
+      </div>
     </div>
   );
 }
-
-export default App;
