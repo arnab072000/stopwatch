88b89600e901acf8fd7b223b9c29f06d629cd81e
arnab072000 <arnab11kr@gmail.com>
2023-12-23 09:41:56 +0530

    "3rd commit"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 
 // function App() {
 //   return (
@@ -24,51 +24,96 @@ import './App.css';
 
 // export default App;
 
-import { useEffect, React, useState } from "react";
+//import { useEffect, React, useState } from "react";
 //import "./styles.css";
 
+// export default function App() {
+//   const [isActivated, setIsActivated] = useState(false);
+//   const [timer, setTimer] = useState(0);
+
+//   useEffect(() => {
+//     let intervalID;
+
+//     if (isActivated) {
+//       intervalID = setInterval(() => {
+//         setTimer((prevTime) => prevTime + 1);
+//       }, 1000);
+//     } else {
+//       clearInterval(intervalID);
+//     }
+//     return () => clearInterval(intervalID);
+//   }, [isActivated]);
+
+//   const startStop = () => {
+//     setIsActivated((prevIsActivated) => !prevIsActivated);
+//   };
+
+//   const reset = () => {
+//     setIsActivated(false);
+//     setTimer(0);
+//   };
+
+//   const formatTime = (seconds) => {
+//     const minutes = Math.floor(seconds / 60);
+//     const remainingSeconds = seconds % 60;
+//     return `${minutes}:${
+//       remainingSeconds < 10 ? "0" : ""
+//     }${remainingSeconds}`;
+//   };
+
+//   return (
+//     <div className="App">
+//       <h1>Stopwatch</h1>
+//       <p> Time : {formatTime(timer)}</p>
+//       <div className='button'>
+//        <button onClick={startStop}>{isActivated ? "Stop" : "Start"}</button>
+//        <button onClick={reset}>Reset</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+import React, { useState, useEffect } from "react";
+
 export default function App() {
-  const [isActivated, setIsActivated] = useState(false);
-  const [timer, setTimer] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
 
   useEffect(() => {
     let intervalID;
 
-    if (isActivated) {
+    if (isRunning) {
       intervalID = setInterval(() => {
-        setTimer((prevTime) => prevTime + 1);
+        setElapsedTime((prevElapsedTime) => prevElapsedTime + 1);
       }, 1000);
     } else {
       clearInterval(intervalID);
     }
-    return () => clearInterval(intervalID);
-  }, [isActivated]);
+
+    return () => clearInterval(intervalID); // Cleanup the interval on unmount
+  }, [isRunning]);
 
   const startStop = () => {
-    setIsActivated((prevIsActivated) => !prevIsActivated);
+    setIsRunning((prevIsRunning) => !prevIsRunning);
   };
 
   const reset = () => {
-    setIsActivated(false);
-    setTimer(0);
+    setIsRunning(false);
+    setElapsedTime(0);
   };
 
   const formatTime = (seconds) => {
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = seconds % 60;
-    return `${minutes}:${
-      remainingSeconds < 10 ? "0" : ""
-    }${remainingSeconds}`;
+    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
   };
 
   return (
-    <div className="App">
+    <div>
       <h1>Stopwatch</h1>
-      <p> Time : {formatTime(timer)}</p>
-      <div className='button'>
-       <button onClick={startStop}>{isActivated ? "Stop" : "Start"}</button>
-       <button onClick={reset}>Reset</button>
-      </div>
+      <p>Time: {formatTime(elapsedTime)}</p>
+      <button onClick={startStop}>{isRunning ? "Stop" : "Start"}</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
